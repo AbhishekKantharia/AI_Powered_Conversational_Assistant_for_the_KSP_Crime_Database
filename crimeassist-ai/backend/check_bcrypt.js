7236c0eb9f435a3dfdd43a0ad4f316f9c7c9b2d7
Abhishek Kantharia <61312706+AbhishekKantharia@users.noreply.github.com>
2026-07-27 01:36:15 +0530

    chore: remove unused configuration file

--- crimeassist-ai/backend/check_bcrypt.js
+++
@@ -1,3 +0,0 @@
-const bcrypt = require('bcryptjs');
-const hash = '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy';
-bcrypt.compare('password', hash).then(r => console.log('bcryptjs match:', r));
